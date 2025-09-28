900f21f81fdfb592ad4be05854f9a6285fc0cf09
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-29 01:52:00 +0600

    added json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { Menu } from 'lucide-react'
 import './App.css'
 import NavBar from './NavBar/NavBar'
+import { Suspense } from 'react'
 
 function App() {
+  const dataPromise = fetch('/data.json')
+    .then(res => res.json())
 
 
   return (
     <>
-      <NavBar></NavBar>
+
+
+      <Suspense fallback={<p>loading...</p>}>
+        <NavBar dataPromise={dataPromise}></NavBar>
+      </Suspense>
 
     </>
   )
--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,19 +1,23 @@
-import { Menu } from 'lucide-react';
-import React from 'react';
+import { Menu, X } from 'lucide-react';
+import React, { use, useState } from 'react';
 
-const NavBar = () => {
+const NavBar = ({ dataPromise }) => {
+    const data = use(dataPromise)
+    const [open, setOpen] = useState(false);
     return (
         <div className='flex justify-between items-center p-7'>
-            <div className='flex items-center justify-center gap-x-4'>
-                <Menu className='md:hidden'></Menu>
+            <div onClick={() => setOpen(!open)} className='flex items-center justify-center gap-x-4'>
+                {
+                    open ? <X className='md:hidden'></X> : <Menu className='md:hidden'></Menu>
+                }
+
                 <h1 className='font-black text-2xl'>Zyra</h1>
             </div>
-            <ul className='flex gap-x-8'>
-                <li>Home</li>
-                <li>Blog</li>
-                <li>Resources</li>
-                <li>Contact</li>
-            </ul>
+            <div className='flex gap-x-8'>
+                {
+                    data.map(dat => <p>{dat.label}</p>)
+                }
+            </div>
             <button className='btn'>Button</button>
         </div>
     );
