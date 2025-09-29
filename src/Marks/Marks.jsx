e8b14cd590c79d861e803a7b9d0883afe4e141b2
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-29 13:44:28 +0600

    added charts

--- src/Marks/Marks.jsx
+++ src/Marks/Marks.jsx
@@ -1,4 +1,5 @@
 import React, { use } from 'react';
+import { Bar, BarChart, XAxis, YAxis } from 'recharts';
 
 const Marks = ({ marksPromise }) => {
     const marksDataRes = use(marksPromise);
@@ -13,14 +14,22 @@ const Marks = ({ marksPromise }) => {
             physics: studentData.physics,
             chemistry: studentData.chemistry,
             math: studentData.math
-
         } 
+        const avg = (student.physics + student.chemistry + student.math) / 3;
+        student.avg = avg;
         return student
     })
     console.log(marksChartData);
     return (
         <div>
-
+            <BarChart width={1500} height={400} data={marksChartData}>
+                <XAxis dataKey='name'></XAxis>
+                <YAxis></YAxis>
+                <Bar dataKey='avg' fill='yellow'></Bar>
+                <Bar dataKey='chemistry' fill='blue'></Bar>
+                <Bar dataKey='physics' fill='red'></Bar>
+                <Bar dataKey='math' fill='white'></Bar>
+            </BarChart>
         </div>
     );
 };
