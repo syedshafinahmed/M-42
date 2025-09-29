860ae3e251970f09b58a3b6a6729124b556e0ca3
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-29 09:23:30 +0600

    added navbar

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -2,20 +2,27 @@ import { Menu, X } from 'lucide-react';
 import React, { use, useState } from 'react';
 
 const NavBar = ({ dataPromise }) => {
-    const data = use(dataPromise)
+    const datas = use(dataPromise)
+    const displayData = datas.map(data => <p className='text-center px-3 hover:bg-amber-700 text-black'>{data.label}</p>)
     const [open, setOpen] = useState(false);
     return (
-        <div className='flex justify-between items-center p-7'>
+        <div className='flex justify-between items-center p-7 '>
             <div onClick={() => setOpen(!open)} className='flex items-center justify-center gap-x-4'>
                 {
                     open ? <X className='md:hidden'></X> : <Menu className='md:hidden'></Menu>
                 }
+                <ul className={`md:hidden absolute duration-1000 bg-amber-500
+                    ${open ? 'top-10 left-15' : '-top-40'}`}>
+                    {
+                        displayData
+                    }
+                </ul>
 
                 <h1 className='font-black text-2xl'>Zyra</h1>
             </div>
-            <div className='flex gap-x-8'>
+            <div className='gap-x-8 md:flex hidden'>
                 {
-                    data.map(dat => <p>{dat.label}</p>)
+                    displayData
                 }
             </div>
             <button className='btn'>Button</button>
